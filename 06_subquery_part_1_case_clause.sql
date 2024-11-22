18d81f974a4d35a74f3bf4c1c5457627680f886e
Jetal Bhanarkar <bhanarkarjetal01@gmail.com>
2024-11-22 16:23:11 +0100

    Update and rename title
    Modified the code for better understanding

--- 06_subquery_part_1_case_clause.sql
+++ 06_subquery_part_1_case_clause.sql
@@ -1,12 +1,12 @@
+-- Mentioning the name of database where we want to write the query
 USE sql_wkday_20240228
 
 -- SUBQUERIES: using output of one query as an input for another query
-
 SELECT TOP 10 * FROM employees
 
 -- fetch the employee getting highest salary
 SELECT MAX(salary)
-FROM employees -- 319254
+FROM employees --output: 319254
 
 
 SELECT * 
@@ -19,9 +19,9 @@ WHERE salary = 319254
 SELECT *  -- outer query
 FROM employees
 WHERE salary = (
-				SELECT MAX(salary) -- inner query
-				FROM employees 
-				)
+		SELECT MAX(salary) -- inner query
+		FROM employees 
+		)
 
 -- the inner query can run on its own
 -- the outer query is dependent on the inner query
@@ -29,112 +29,102 @@ WHERE salary = (
 -- SCALAR SUBQUERY: returns a single value
 
 -- fetch the employee who earns the minium salary
-
 SELECT *  -- outer query
 FROM employees
 WHERE salary = (
-				SELECT MIN(salary) -- inner query
-				FROM employees 
-				)
+		SELECT MIN(salary) -- inner query
+		FROM employees 
+		)
 
 -- find the employee from SD-REPORT department who earns the max salary
-SELECT *  -- outer query
+SELECT *  
 FROM employees
-WHERE salary =
-				(
-				SELECT MAX (salary) -- inner query
-				FROM employees 
-				WHERE employee_dept = 'SD-Report'
-				)
+WHERE salary = (
+		SELECT MAX (salary) 
+		FROM employees 
+		WHERE employee_dept = 'SD-Report'
+		)
 AND employee_dept = 'SD-Report'
 
 -- all the employees getting minimum salary from SD-Infra dept
-
-SELECT *  -- outer query
+SELECT * 
 FROM employees
-WHERE salary =
-				(
-				SELECT MIN (salary) -- inner query
-				FROM employees 
-				WHERE employee_dept = 'SD-Infra'
-				)
+WHERE salary = (
+		SELECT MIN (salary) 
+		FROM employees 
+		WHERE employee_dept = 'SD-Infra'
+		)
 AND employee_dept = 'SD-Infra'
 
 -- fetch all the employees from entire organization who earns more than avg salary
 SELECT *  -- outer query
 FROM employees
-WHERE salary >
-				(
-				SELECT AVG (salary) -- inner query
-				FROM employees 
-				)
+WHERE salary > (
+		SELECT AVG (salary) -- inner query
+		FROM employees 
+		)
 ORDER BY salary DESC
 
 -- find all the employees from the entire organization who earns between 50% and 100% of the max salary of SD-WEB
-SELECT *  -- outer query
+SELECT *  
 FROM employees
-WHERE salary BETWEEN 
-					(
-					SELECT 0.5*MAX (salary) -- inner query
-					FROM employees 
-					WHERE employee_dept = 'SD-WEB'
-					)
-			AND 
-				(
-				SELECT MAX (salary) -- inner query
-				FROM employees 
-				WHERE employee_dept = 'SD-WEB'
-				)
+WHERE salary BETWEEN (
+			SELECT 0.5*MAX (salary)
+			FROM employees 
+			WHERE employee_dept = 'SD-WEB'
+			)
+		AND 
+			(
+			SELECT MAX (salary)
+			FROM employees 
+			WHERE employee_dept = 'SD-WEB'
+			)
 ORDER BY salary DESC
 
 
-SELECT * , 89721 AS avg_sal -- outer query
+SELECT * , 89721 AS avg_sal 
 FROM employees
-WHERE salary >
-				(
-				SELECT AVG (salary) -- inner query
-				FROM employees 
-				)
+WHERE salary > (
+		SELECT AVG (salary) -- inner query
+		FROM employees 
+		)
 ORDER BY salary DESC
 
 -- we can add any column of our choice in the table
-
-SELECT * , (SELECT AVG (salary) 
-			FROM employees ) 
+SELECT * , 
+	(SELECT AVG (salary) 
+	FROM employees ) 
 FROM employees
-WHERE salary >
-				(
-				SELECT AVG (salary) -- inner query
-				FROM employees 
-				)
+WHERE salary > (
+		SELECT AVG (salary) -- inner query
+		FROM employees 
+		)
 ORDER BY salary DESC
 
 -- find all the employees from the entire organization who earns 
 --between 50% and 90% of the max salary of SD-WEB
 SELECT * , (
-			SELECT 0.5*MAX (salary) -- inner query
-			FROM employees 
-			WHERE employee_dept = 'SD-WEB'
-			) AS fifty_pc_sal,
-			
-			(
-			SELECT 0.9*MAX (salary) -- inner query
-			FROM employees 
-			WHERE employee_dept = 'SD-WEB'
-			) AS ninety_pc_sal
+		SELECT 0.5*MAX (salary) -- inner query
+		FROM employees 
+		WHERE employee_dept = 'SD-WEB'
+		) AS fifty_pc_sal,
+		(
+		SELECT 0.9*MAX (salary) -- inner query
+		FROM employees 
+		WHERE employee_dept = 'SD-WEB'
+		) AS ninety_pc_sal
 FROM employees
-WHERE salary BETWEEN 
-					(
-					SELECT 0.5*MAX (salary) -- inner query
-					FROM employees 
-					WHERE employee_dept = 'SD-WEB'
-					)
-			AND 
-				(
-				SELECT 0.9*MAX (salary) -- inner query
-				FROM employees 
-				WHERE employee_dept = 'SD-WEB'
-				)
+WHERE salary BETWEEN (
+		SELECT 0.5*MAX (salary) -- inner query
+		FROM employees 
+		WHERE employee_dept = 'SD-WEB'
+		)
+		AND 
+		(
+		SELECT 0.9*MAX (salary) -- inner query
+		FROM employees 
+		WHERE employee_dept = 'SD-WEB'
+		)
 ORDER BY salary DESC
 
 
@@ -143,11 +133,11 @@ ORDER BY salary DESC
 -- if a person earns greater than or equal to avg salary, classify him as 'High-Sal'
 
 SELECT *,
-		CASE
-			WHEN salary < (SELECT AVG (salary) FROM employees)
-			THEN 'Low-Sal'
-			ELSE 'High-Sal'
-		END AS comment
+	CASE
+		WHEN salary < (SELECT AVG (salary) FROM employees)
+		THEN 'Low-Sal'
+		ELSE 'High-Sal'
+	END AS comment
 FROM employees
 
 -- if an employee get salary lower than 80% of the avg salary>> Low-Sal
@@ -155,14 +145,14 @@ FROM employees
 -- if an employee get salary > 200% of avg salary >> High-Sal
 
 SELECT *,
-		CASE
-			WHEN salary < 0.8*(SELECT AVG (salary) FROM employees)
-			THEN 'Low-Sal'
-			WHEN salary BETWEEN 0.8*(SELECT AVG (salary) FROM employees)
-						AND 2*(SELECT AVG (salary) FROM employees)
-			THEN 'Avg_Sal'
-			ELSE 'High-Sal'
-		END AS comment
+	CASE
+		WHEN salary < 0.8*(SELECT AVG (salary) FROM employees)
+		THEN 'Low-Sal'
+		WHEN salary BETWEEN 0.8*(SELECT AVG (salary) FROM employees)
+				AND 2*(SELECT AVG (salary) FROM employees)
+		THEN 'Avg_Sal'
+		ELSE 'High-Sal'
+	END AS comment
 FROM employees
 
 -- get all the employees who belong to the top two dept with most employees
@@ -181,14 +171,15 @@ ORDER BY COUNT(*)  DESC
 SELECT * 
 FROM employees
 WHERE employee_dept IN ('SD-WEB','SD-REPORT')
-ORDER BY employee_dept, salary DESC
+ORDER BY employee_dept, 
+	salary DESC
 
 -- OR
 
 SELECT * 
 FROM employees
 WHERE employee_dept -- one column but miltiple rows, therefore, we use IN command
-	IN (
+	IN 	(
 		SELECT TOP 2 employee_dept 
 		FROM employees
 		GROUP BY employee_dept
@@ -210,18 +201,19 @@ WHERE employee_dept IN
 
 
 -- max salary of every department
-SELECT employee_dept, MAX(salary) AS max_sal
+SELECT employee_dept, 
+	MAX(salary) AS max_sal
 FROM employees
 GROUP BY employee_dept
 ORDER BY max_sal DESC
 
-
+-- fetch top 2 employee dept based on maximum salary
 SELECT *
 FROM employees
 WHERE employee_dept IN
-					(
-					SELECT TOP 2 employee_dept
-					FROM employees
-					GROUP BY employee_dept
-					ORDER BY MAX(salary) DESC
-					)
+			(
+			SELECT TOP 2 employee_dept
+			FROM employees
+			GROUP BY employee_dept
+			ORDER BY MAX(salary) DESC
+			)
